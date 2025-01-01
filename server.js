03449bcb13c6b9b610d574a3361d54ade32136a9
Cyrus <kur0dk0@gmail.com>
2025-01-01 14:56:16 +0700

    fix: fix conflict branch

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { app } from './src/app.js';
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`Example app listening on http://127.0.0.1:${port}`);
