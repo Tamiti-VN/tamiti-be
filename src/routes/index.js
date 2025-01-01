66a0514963e868571ec9635ae86a2383f521a7e6
Cyrus <kur0dk0@gmail.com>
2025-01-01 15:21:56 +0700

    refactor(framework): change express to nestjs

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { routesV1 } from "./v1/index.js";
-const router = express.Router();
-
-router.use("/v1", routesV1);
-
-export { router as api };
--- src/routes/v1/order.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getComment } from "../../controllers/v1/comment.js";
-
-const commentRoutes = express.Router();
-commentRoutes.get("/", getComment);
-
-export { commentRoutes };
